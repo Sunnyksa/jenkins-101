42421b5bcb68db59326e9a71872241f17b19bf8e
Sunnyksa <168031925+Sunnyksa@users.noreply.github.com>
2024-05-14 23:34:18 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,2 @@
 
-print("Hello my friends, jenkins is cool but its technical but you have to pay a lot of attention or else you are lost. 
-trying to trigger a job on jenkins e no easy o. to get 6 pack")
+print("Hello my friends, jenkins is cool but its technical but you have to pay a lot of attention or else you are lost. trying to trigger a job on jenkins e no easy o. to get 6 pack")
